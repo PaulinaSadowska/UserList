73997111fc7262852de090b25717253917d60223
Paulina Sadowska <paulina.sadowska@student.put.poznan.pl>
2016-07-23 13:16:14 +0200

    app appearance changed

--- app/src/main/java/com/nekodev/paulina/sadowska/userlist/UserViewHolder.java
+++ app/src/main/java/com/nekodev/paulina/sadowska/userlist/UserViewHolder.java
@@ -2,7 +2,6 @@ package com.nekodev.paulina.sadowska.userlist;
 
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
-import android.widget.ImageView;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
@@ -10,6 +9,7 @@ import com.nekodev.paulina.sadowska.userlist.listeners.ItemClickedListener;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import de.hdodenhof.circleimageview.CircleImageView;
 
 /**
  * Created by Paulina Sadowska on 21.07.2016.
@@ -17,7 +17,7 @@ import butterknife.ButterKnife;
 public class UserViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
 
     @BindView(R.id.user_avatar)
-    ImageView userAvatar;
+    CircleImageView userAvatar;
     @BindView(R.id.user_name)
     TextView userName;
     @BindView(R.id.user_item_layout)
